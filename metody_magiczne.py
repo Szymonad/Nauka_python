e7e459d636aed226408d0ff30c500255a09f3db6
Szymonad <szymonad-1999@o2.pl>
2024-10-24 20:32:05 +0200

    aktualna wersja

---
+++ metody_magiczne.py
@@ -0,0 +1,14 @@
+# rzykład
+def __add__(self, other):
+    return self.value + other.value
+
+
+# __sub__(self, other) – Obsługa operatora - dla odejmowania.
+#
+# __mul__(self, other) – Obsługa operatora * dla mnożenia.
+#
+# __truediv__(self, other) – Obsługa operatora / dla dzielenia.
+#
+# __mod__(self, other) – Obsługa operatora % dla operacji modulo.
+#
+# __pow__(self, other) – Obsługa operatora ** dla potęgowania.
